b1cf771c6894c598b234eb9b81cd7b58ec5861bd
unknown <cameron@floids.co>
2024-02-16 18:09:49 +0530

    Most control panel features and responsiveness complete

--- assets/scripts.js
+++ assets/scripts.js
@@ -12,33 +12,43 @@ document.addEventListener("DOMContentLoaded", function () {
         const queueHeader = document.getElementById("queueHeader");
         const queueCells = document.querySelectorAll('.queue');
         const priorityCells = document.querySelectorAll('.priority');
-
+        const numberOfQueuesGroup = document.getElementById("number-of-queues-group");
+        const queueAttributesGroup = document.getElementById("queue-attributes-group");
+    
         if (selectedAlgorithm === "fcfs" || selectedAlgorithm === "sjf" || selectedAlgorithm === "srjf") {
             // For algorithms where priority and queue are not used
             priorityHeader.style.display = "none";
             queueHeader.style.display = "none";
             queueCells.forEach(cell => cell.style.display = "none");
             priorityCells.forEach(cell => cell.style.display = "none");
+            numberOfQueuesGroup.style.display = "none";
+            queueAttributesGroup.style.display = "none";
         } else if (selectedAlgorithm === "priority") {
             // For Priority algorithm
             priorityHeader.style.display = "table-cell";
             queueHeader.style.display = "none";
             queueCells.forEach(cell => cell.style.display = "none");
             priorityCells.forEach(cell => cell.style.display = "table-cell");
+            numberOfQueuesGroup.style.display = "none";
+            queueAttributesGroup.style.display = "none";
         } else if (selectedAlgorithm === "rr") {
             // For Round Robin algorithm
             priorityHeader.style.display = "none";
             queueHeader.style.display = "none";
             queueCells.forEach(cell => cell.style.display = "none");
             priorityCells.forEach(cell => cell.style.display = "none");
+            numberOfQueuesGroup.style.display = "none";
+            queueAttributesGroup.style.display = "none";
         } else if (selectedAlgorithm === "mq") {
             // For Multi Queue algorithm
             priorityHeader.style.display = "none";
             queueHeader.style.display = "table-cell";
             queueCells.forEach(cell => cell.style.display = "table-cell");
             priorityCells.forEach(cell => cell.style.display = "none");
+            numberOfQueuesGroup.style.display = "block";
+            queueAttributesGroup.style.display = "block";
         }
-    }
+    }    
 
     // Function to toggle visibility of the time quantum group
     function toggleTimeQuantumGroup(selectedAlgorithm) {
@@ -67,25 +77,25 @@ document.addEventListener("DOMContentLoaded", function () {
 
     // Function to add more processes to the processes input table
     const nProcessesInput = document.querySelector('input[name="nprocesses"]');
-    const tableBody = document.querySelector('tbody');
+    const processAttributesBody = document.getElementById('process-attributes-body');
     nProcessesInput.addEventListener("change", function () {
         // Get the number of processes entered by the user
         const numberOfProcesses = parseInt(nProcessesInput.value);
 
         // Clear existing rows from the table body
-        tableBody.innerHTML = '';
+        processAttributesBody.innerHTML = '';
 
         // Add new rows based on the number of processes
         for (let i = 1; i <= numberOfProcesses; i++) {
             const newRow = document.createElement('tr');
             newRow.innerHTML = `
                 <td>P${i}</td>
-                <td><input type="number" name="arrival_time_${i}"></td>
-                <td><input type="number" name="burst_time_${i}"></td>
-                <td class="priority" style="display: none;"><input type="number" name="priority_${i}"></td>
-                <td class="queue" style="display: none;"><input type="number" name="queue_${i}"></td>
+                <td><input type="number" name="arrival_time_${i}" min="0"></td>
+                <td><input type="number" name="burst_time_${i}" min="0"></td>
+                <td class="priority" style="display: none;"><input type="number" name="priority_${i}" min="0"></td>
+                <td class="queue" style="display: none;"><input type="number" name="queue_${i}" min="0"></td>
             `;
-            tableBody.appendChild(newRow);
+            processAttributesBody.appendChild(newRow);
         }
 
         // Get selected algorithm
@@ -96,6 +106,35 @@ document.addEventListener("DOMContentLoaded", function () {
 
         // Toggle visibility of time quantum group
         toggleTimeQuantumGroup(selectedAlgorithm);
+    });
+
+    // Function to add queue attributes rows based on the number of queues
+    const nQueuesInput = document.getElementById('nqueues');
+    const queueAttributesBody = document.getElementById('queue-attributes-body');
+    nQueuesInput.addEventListener("change", function () {
+        const numberOfQueues = parseInt(nQueuesInput.value);
+
+        // Clear existing rows from the table body
+        queueAttributesBody.innerHTML = '';
+
+        // Add new rows based on the number of queues
+        for (let i = 1; i <= numberOfQueues; i++) {
+            const newRow = document.createElement('tr');
+            newRow.innerHTML = `
+                <td>Q${i}</td>
+                <td>
+                    <select name="algorithm_q${i}">
+                        <option value="fcfs" selected>FCFS</option>
+                        <option value="sjf">SJF</option>
+                        <option value="srjf">SRJF</option>
+                        <option value="priority">Priority</option>
+                        <option value="rr">RR</option>
+                    </select>
+                </td>
+                <td><input type="number" name="priority_q${i}" min="0"></td>
+                `;
+            queueAttributesBody.appendChild(newRow); // Append rows to the queue attributes table body
+        }
     });
 
     // Call the function to set the current year
